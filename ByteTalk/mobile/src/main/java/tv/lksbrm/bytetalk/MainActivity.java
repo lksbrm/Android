76a49b60f71e458799af5282374b36b0464e68ad
lukas <lukas@Lukas-Laptop>
2016-02-01 13:24:41 +0100

    Addded MongoDB Service

--- ByteTalk/mobile/src/main/java/tv/lksbrm/bytetalk/MainActivity.java
+++ ByteTalk/mobile/src/main/java/tv/lksbrm/bytetalk/MainActivity.java
@@ -2,12 +2,46 @@ package tv.lksbrm.bytetalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    public static MongoClient mc;
+    TextView tv;
+    MongoManager mm = new MongoManager();
+
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init()
+    {
+        tv = (TextView) findViewById(R.id.textView);
+        try
+        {
+
+            mc = new MongoClient(new MongoClientURI("mongodb://192.168.178.27:28017"));
+            mm.init();
+            tv.setText("Erfolgreich zur Datenbank verbunden!");
+
+
+        }catch(UnknownHostException e)
+        {
+            tv.setText("Fehler beim Verbinden zur Datenbank!");
+        }
     }
 }
