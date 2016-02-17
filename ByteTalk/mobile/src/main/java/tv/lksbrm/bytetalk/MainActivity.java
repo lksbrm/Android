41ba24f4ecfe3727e2a73fe8bc835c39948df2d6
lksbrm <lukas.briem@hotmail.de>
2016-02-17 14:38:57 +0100

    Added UserActivity

--- ByteTalk/mobile/src/main/java/tv/lksbrm/bytetalk/MainActivity.java
+++ ByteTalk/mobile/src/main/java/tv/lksbrm/bytetalk/MainActivity.java
@@ -1,12 +1,18 @@
 package tv.lksbrm.bytetalk;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -14,16 +20,26 @@ import com.mongodb.MongoCredential;
 
 import java.net.UnknownHostException;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 
     public static MongoClient mc;
     TextView tv;
     TextView tv2;
     Button reg;
+    Button login;
     static MongoManager mm = new MongoManager();
 
+    EditText username;
+    EditText password;
 
+    Toolbar toolbar;
+
+
+    public static SharedPreferences prefs;
+    public static SharedPreferences.Editor prefseditor;
+
+    public LoginManager lm;
 
 
     public static MongoManager getMongoManager()
@@ -32,28 +48,92 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+    public void goToUserScreen()
     {
+
+            Intent in = new Intent(MainActivity.this, UserActivity.class);
+            startActivity(in);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         init();
 
+        lm = new LoginManager(this);
 
-        reg.setOnClickListener(new View.OnClickListener()
+        username = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+
+        login = (Button) findViewById(R.id.button);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("ByteTalk");
+        toolbar.setSubtitle("Bitte logge dich ein!");
+
+        prefs = this.getSharedPreferences("prefslogin", MODE_PRIVATE);
+        prefseditor = prefs.edit();
+
+        login.setOnClickListener(this);
+        reg.setOnClickListener(this);
+
+        Thread t = new Thread(new Runnable()
         {
             @Override
-            public void onClick(View v)
+            public void run()
             {
-
-                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(i);
+                String s = "Anzahl: " + mm.collection.find().size();
+                tv.setText(s);
             }
         });
+        t.start();
+
+
+
 
 
     }
+
+       @Override
+       public void onClick(View v)
+       {
+
+           switch(v.getId())
+           {
+               case R.id.button:
+               {
+                   if(lm.userExists("test"))
+                   {
+                       Toast.makeText(getApplicationContext(), "User exists", Toast.LENGTH_SHORT);
+                   }
+                   if(lm.B)
+                   {
+                       goToUserScreen();
+                   }else
+                   {
+                       Toast.makeText(getApplicationContext(), "Login invalid!", Toast.LENGTH_SHORT);
+                   }
+                   break;
+               }
+               case R.id.button2:
+               {
+
+                   Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                   startActivity(i);
+                   break;
+               }
+
+
+           }
+
+
+       }
 
     public void init()
     {
@@ -72,5 +152,11 @@ public class MainActivity extends AppCompatActivity
         {
             tv.setText("Fehler beim Verbinden zur Datenbank!");
         }
+    }
+
+    public void setText(String text)
+    {
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(text);
     }
 }
--- ByteTalk/mobile/src/main/java/tv/lksbrm/bytetalk/MongoManager.java
+++ ByteTalk/mobile/src/main/java/tv/lksbrm/bytetalk/MongoManager.java
@@ -15,6 +15,7 @@ public class MongoManager
     DB db;
     DBCollection collection;
 
+
     public void init()
     {
         db = MainActivity.mc.getDB("user");
@@ -22,29 +23,69 @@ public class MongoManager
 
     }
 
-    public void createUser(String email, String name, String password)
+    public void createUser(final String email, final String name, final String password)
     {
-        DBObject user = new BasicDBObject("name", name)
-                         .append("password", password)
-                         .append("email", email);
 
-        collection.insert(user);
-        i = q/t;
+
+        Thread network = new Thread(new Runnable()
+       {
+
+           @Override
+           public void run()
+           {
+
+
+               collection.insert((DBObject) new BasicDBObject("name", name)
+                       .append("password", password)
+                       .append("email", email));
+
+           }
+       });
+       network.start();
+
+    }
+    DBObject RES = new BasicDBObject("email", "").append("name", "");
+    public DBObject getUser(final String name)
+    {
+        Thread nete = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                System.out.println(collection.find().size());
+                for(DBObject all : collection.find())
+                {
+                    if(all.get("name") == name)
+                    {
+                        RES = all;
+                    }
+                }
+
+            }
+        });
+
+        return RES;
+
+
     }
 
-    public DBObject getUser(String name) {
-        DBObject query = new BasicDBObject("name", name);
-        DBCursor cursor = collection.find(query);
-        DBObject result = cursor.one();
+    DBObject RES2 = new BasicDBObject("email", "").append("name", "");
+    public DBObject getUserByEmail(final String email)
+    {
+      Thread nete = new Thread(new Runnable()
+      {
+          @Override
+          public void run()
+          {
+              DBObject query = new BasicDBObject("email", email);
+              DBCursor cursor = collection.find(query);
+              RES2 = cursor.one();
 
-        return result;
-    }
+          }
+      });
 
-    public DBObject getUserByEmail(String email) {
-        DBObject query = new BasicDBObject("email", email);
-        DBCursor cursor = collection.find(query);
-        DBObject result = cursor.one();
+          return RES2;
 
-        return result;
+
     }
     }
